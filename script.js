84f516b8019bba26f2ad479fb0c2ab1adb5ee17e
nlandau-0789 <84878415+nlandau-0789@users.noreply.github.com>
2022-03-27 22:03:08 +0200

    lol added everything

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+String.prototype.trim = function() 
+    {
+        temp = String(this).replace("(", "").replace(")", "").replace(" ", "").replace("/","");
+        return temp;
+    };
+function insererMoyenne(matiere){
+    var notes = matiere.children[3].children;
+    var moyenne = matiere.children[2];
+    var num = 0;
+    var denom = 0;
+    for (var i = 0; i < notes.length; i++) {
+        var note = parseFloat(notes[i].querySelector("span.valeur").innerText.replace(",",".").split(" ")[0]);
+        if (notes[i].querySelector("sub")) {
+            note /= parseFloat(notes[i].querySelector("sub").innerText.trim());
+        } else {
+            note /= 20;
+        }
+        if (notes[i].querySelector("sup")) {
+            var coeff = parseFloat(notes[i].querySelector("sup").innerText.trim());
+        } else {
+            var coeff = 1;
+        }
+        num += note * coeff;
+        denom += coeff;
+    }
+    moyenne.innerText = (num / denom) * 20;
+}
 
+matieres = document.querySelectorAll("tr.ng-star-inserted");
+
+for (var i = 0; i < matieres.length-1; i++) {
+    insererMoyenne(matieres[i]);
+}
